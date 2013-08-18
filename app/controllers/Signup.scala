8836f46a2ade221c32df7590b9fd6a71c19038a0
frankdavid <info@frankdavid.hu>
2013-08-19 00:03:30 +0200

    Delete Signup.scala

--- app/controllers/Signup.scala
+++
@@ -1,9 +0,0 @@
-package controllers
-
-import play.api.mvc._
-
-object Signup extends Controller {
-  def main(args: Array[String]) = {
-    List(2, 3, 50, 100, 40, 20, 120, 20).map(_ * 3).filter(_ < 200).foreach(println)
-  }
-}
